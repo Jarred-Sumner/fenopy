aa0d0a5a97801294104ad038ad64da63c2144d96
Jarred Sumner <jarred@jarredsumner.com>
2012-09-28 14:25:10 -0700

    Working

--- lib/fenopy/torrent.rb
+++ lib/fenopy/torrent.rb
@@ -3,15 +3,16 @@ module Fenopy
 		attr_accessor :name, :size, :page, :url, :magnet, :hash, :category, :seeders, :leechers
 
 		def initialize(options)
-			self.name 		= options[:name]
-			self.size 		= options[:size]
-			self.page 		= options[:page]
-			self.url 			= options[:torrent]
-			self.magnet 	= options[:magnet]
-			self.hash 		= options[:hash]
-			self.category = options[:category]
-			self.seeders  = options[:seeder]
-			self.leechers = options[:leecher]
+			puts options.inspect
+			self.name 		= options['name']
+			self.size 		= options['size']
+			self.page 		= options['page']
+			self.url 			= options['torrent']
+			self.magnet 	= options['magnet']
+			self.hash 		= options['hash']
+			self.category = options['category']
+			self.seeders  = options['seeder']
+			self.leechers = options['leecher']
 		end
 
 	end
